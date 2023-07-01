3579d0b208fbb4ad9b55e38da731cfd9e5e45608
spoorthi <spoorthiachar9@gmail.com>
2023-07-01 10:46:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Post from './post';
+import Header from './Header';
+import {Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route index element={
+         <main>
+         <Header/> 
+         <Post/>
+         <Post/>
+         <Post/>
+      </main>
+      }/>
+      <Route path={'./login'}element={
+        <div>login</div>
+      }/>
+     </Routes>
   );
 }
 
---
+++ src/post.js
@@ -0,0 +1,18 @@
+export default function post()
+{
+    return(
+        <div className="post">
+      <div className="image">
+      <img src="https://th.bing.com/th/id/OIP.KTfMOENFVRTD03etYjbuggHaE8?pid=ImgDet&rs=1" alt=""></img>
+      </div>
+      <div className="texts">
+      <h3>In 1914, Manchester was one of the biggest motor manufacturing cities in the world. Two decades later cars had become part of our everyday lives—but Manchester’s dominance had faded.</h3>
+      <p className="info">
+        <a className="author">Dawid Pasko </a>
+        <time>2023-01--06 16:45</time>
+      </p>
+      <p className="summary">By the start of the First World War, Manchester boasted a prosperous motor manufacturing industry with several important firms. These included Ford, who opened their first factory outside of the US at Trafford Park in 1911; the Belsize Motor Company, Manchester’s largest native motorcar manufacturer (their popular 10/12 HP Belsize is pictured below); </p>
+      </div>
+      </div>
+    );
+}
